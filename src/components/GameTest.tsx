7caaf58a00108dde127727c6849cb89d29e1188b
amithcabraal <147496214+amithcabraal@users.noreply.github.com>
2025-01-21 15:19:39 +0000

    Update GameTest.tsx

--- src/components/GameTest.tsx
+++ src/components/GameTest.tsx
@@ -4,7 +4,7 @@ const GameTest: React.FC = () => {
   return (
     <div className="w-full h-[calc(100vh-4rem)] bg-gray-900">
       <iframe
-        src="https://corsproxy.io/https://www.national-lottery.co.uk/iwgTry/loadHTML5Game?ticketId=3139517335&iwgSeoName=merge-meter"
+        src="https://dev.instantrgs.com/irgs/QA/allwyn/"
         className="w-full h-full border-0"
         allow="fullscreen"
         title="Game Test"
